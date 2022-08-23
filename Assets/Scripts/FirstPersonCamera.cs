ea650ae783a1ff44ade931906bdfbba60393f68d
Adão Ribeiro <adao-eduardo@hotmail.com>
2022-08-23 10:30:42 -0300

    FirstPersonCamera

---
+++ Assets/Scripts/FirstPersonCamera.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+public class FirstPersonCamera : MonoBehaviour
+{
+    [Header("Transform")]
+    public Transform characterBody;
+    public Vector2 sensitivity = new Vector2(0.5f, 0.5f);
+    public Vector2 smoothing = new Vector2(3, 3);
+
+    private Vector2 clampInDegrees = new Vector2(360, 180);
+
+    [SerializeField] private float rotationX = 0;
+    [SerializeField] private float rotationY = 0;
+    [SerializeField] private float angleYmin = -90;
+    [SerializeField] private float angleYmax = 90;
+
+    bool lockCursor;
+
+    [HideInInspector]
+    public Vector2 targetDirection;
+
+    public LayerMask defaultLayer;
+
+    private void Start()
+    {
+
+        defaultLayer = GetComponent<Camera>().cullingMask;
+    }
+    public void ActiveCursor(bool active)
+    {
+        lockCursor = active;
+        Cursor.visible = active;
+        Cursor.lockState = active ? CursorLockMode.None : CursorLockMode.Locked;
+    }
+    public void UpdateCamera()
+    {
+        if (lockCursor)
+        {
+            return;
+        }
+
+        Quaternion targetOrientation = Quaternion.Euler(targetDirection);
+
+        rotationX += InputManager.Instance.GetMouseDelta().x * sensitivity.x;
+        rotationY += InputManager.Instance.GetMouseDelta().y * sensitivity.y;
+
+        rotationY = Mathf.Clamp(rotationY, angleYmin, angleYmax);
+
+        characterBody.localRotation = Quaternion.Euler(0, rotationX, 0);
+        transform.localRotation = Quaternion.Euler(-rotationY, 0, 0);
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + transform.forward * 1000);
+    }
+
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     Moviment moviment;
+    FirstPersonCamera Camera;
     // Start is called before the first frame update
     void Start()
     {
         moviment = GetComponent<Moviment>();
+        Camera = GetComponentInChildren<FirstPersonCamera>();
     }
 
     // Update is called once per frame
     void Update()
     {
         moviment.UpdateMoviment();
+        Camera.UpdateCamera();
     }
 }
